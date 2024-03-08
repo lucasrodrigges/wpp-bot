7935e6c6c8f4c2b56ab69b80fc3b1538f46dc8c4
Lucas <rodriggeslucas@gmail.com>
2024-03-08 06:10:20 -0300

    Update INSTALAR.cmd script to install nvm and set environment variables

--- INSTALAR.cmd
+++ INSTALAR.cmd
@@ -1,32 +1,30 @@
 mkdir .\tools
 
-call curl -L https://github.com/microsoft/winget-cli/releases/download/v1.7.10582/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -o .\tools\winget.msixbundle
-.\tools\winget.msixbundle
+@REM call curl -L https://github.com/microsoft/winget-cli/releases/download/v1.7.10582/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -o .\tools\winget.msixbundle
+@REM .\tools\winget.msixbundle
 
-call winget install --id Git.Git -e --source winget
+@REM call winget install --id Git.Git -e --source winget
 
-call git -v
+call curl -L https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe -o .\tools\nvm.exe
+.\tools\nvm.exe
 
-@REM call curl -L https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe -o .\tools\nvm.exe
-@REM .\tools\nvm.exe
+@echo off
+set NVM_HOME=C:\Users\%USERNAME%\AppData\Roaming\nvm
+set NVM_SYMLINK=C:\Program Files\nodejs
+setx /M NVM_HOME "%NVM_HOME%"
+setx /M NVM_SYMLINK "%NVM_SYMLINK%"
 
-@REM @echo off
-@REM set NVM_HOME=C:\Users\%USERNAME%\AppData\Roaming\nvm
-@REM set NVM_SYMLINK=C:\Program Files\nodejs
-@REM setx /M NVM_HOME "%NVM_HOME%"
-@REM setx /M NVM_SYMLINK "%NVM_SYMLINK%"
+echo PATH=%PATH% > %NVM_HOME%\PATH.txt
 
-@REM echo PATH=%PATH% > %NVM_HOME%\PATH.txt
+for /f "skip=2 tokens=2,*" %%A in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path 2^>nul') do (
+  setx /M PATH "%%B;%%NVM_HOME%%;%%NVM_SYMLINK%%"
+)
 
-@REM for /f "skip=2 tokens=2,*" %%A in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path 2^>nul') do (
-@REM   setx /M PATH "%%B;%%NVM_HOME%%;%%NVM_SYMLINK%%"
-@REM )
+if exist "%SYSTEMDRIVE%\Program Files (x86)\" (
+set SYS_ARCH=64
+) else (
+set SYS_ARCH=32
+)
+(echo root: %NVM_HOME% && echo path: %NVM_SYMLINK% && echo arch: %SYS_ARCH% && echo proxy: none) > %NVM_HOME%\settings.txt
 
-@REM if exist "%SYSTEMDRIVE%\Program Files (x86)\" (
-@REM set SYS_ARCH=64
-@REM ) else (
-@REM set SYS_ARCH=32
-@REM )
-@REM (echo root: %NVM_HOME% && echo path: %NVM_SYMLINK% && echo arch: %SYS_ARCH% && echo proxy: none) > %NVM_HOME%\settings.txt
-
-@REM @echo on
+@echo on
