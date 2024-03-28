9c5bf330b7eac57b1aef923d99b5f52a25c713ee
lucasrodrigges <rodriggeslucas@gmail.com>
2024-03-28 16:53:56 -0300

    Add contact files for each batch of 500 contacts

--- src/scripts/getNumbers.js
+++ src/scripts/getNumbers.js
@@ -9,8 +9,22 @@ const path = require('path');
     .map((contact) => ({
       name: contact['First Name'].split(' ')[0] || contact['Display Name'].split(' ')[0],
       phone: contact['Mobile Phone'] || contact['Home Phone'] || contact['Work Phone'] || contact['Other Phone'],
-    })).filter((contact) => contact.phone && contact.name);
+    })).filter((contact) => contact.phone && contact.name)
+    .sort((a, b) => a.name.localeCompare(b.name));
   fs.writeFileSync(path.join(__dirname, '..', '..', 'arquivos', 'contatosSemTantra.json'), JSON.stringify(valid, null, 2));
-  const txt = valid.map((contact) => `${contact.name}, ${contact.phone}`).join('\n');
-  fs.writeFileSync(path.join(__dirname, '..', '..', 'arquivos', 'contatos.txt'), txt);
+
+  // create a single file with all contacts
+  // const txt = valid.map((contact) => `${contact.name}, ${contact.phone}`).join('\n');
+  // fs.writeFileSync(path.join(__dirname, '..', '..', 'arquivos', 'contatos.txt'), txt);
+
+  // create a file for each 200 contacts
+  const contactsPerFile = 500;
+  const files = Math.ceil(valid.length / contactsPerFile);
+  for (let i = 0; i < files; i++) {
+    const start = i * contactsPerFile;
+    const end = (i + 1) * contactsPerFile;
+    const thisContacts = valid.slice(start, end);
+    const newTxt = thisContacts.map((contact) => `${contact.name}, ${contact.phone}`).join('\n');
+    fs.writeFileSync(path.join(__dirname, '..', '..', 'arquivos', `contatos${i}.txt`), newTxt);
+  }
 })();
